5b1b4243a247ba25bfb1822e4be4638521e37d03
Miroslav Valov <91765698+mivalov@users.noreply.github.com>
2025-10-20 01:22:48 +0200

    Update optional git config

--- home/mvalov/optional/git.nix
+++ home/mvalov/optional/git.nix
@@ -7,11 +7,14 @@
     enable = true;
     userName = lib.mkDefault "Miroslav Valov";
     userEmail = lib.mkDefault "91765698+mivalov@users.noreply.github.com";
+    aliases = {
+      logs = "--graph --all --abbrev-commit --pretty=format:'%C(auto)%h%Creset -%C(auto)%d%Creset %s %C(brightyellow)(%cr) %C(bold brightblue)[%an]%Creset'";
+    };
     extraConfig = {
-      init.defaultBranch = "main";
-      core.editor = "vim";
-      core.autocrlf = "input";
-      pull.ff = "only";
+      init.defaultBranch = lib.mkDefault "main";
+      core.editor = lib.mkDefault "vim";
+      core.autocrlf = lib.mkDefault "input";
+      pull.ff = lib.mkDefault "only";
     };
   };
 }
