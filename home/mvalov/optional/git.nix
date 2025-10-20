3310192759a46d29b66ccbc74a03c7bcdb4b147f
Miroslav Valov <91765698+mivalov@users.noreply.github.com>
2025-10-20 02:03:50 +0200

    Fix git alias command

--- home/mvalov/optional/git.nix
+++ home/mvalov/optional/git.nix
@@ -8,7 +8,7 @@
     userName = lib.mkDefault "Miroslav Valov";
     userEmail = lib.mkDefault "91765698+mivalov@users.noreply.github.com";
     aliases = {
-      logs = "--graph --all --abbrev-commit --pretty=format:'%C(auto)%h%Creset -%C(auto)%d%Creset %s %C(brightyellow)(%cr) %C(bold brightblue)[%an]%Creset'";
+      logs = "log --graph --all --abbrev-commit --pretty=format:'%C(auto)%h%Creset -%C(auto)%d%Creset %s %C(brightyellow)(%cr) %C(bold brightblue)[%an]%Creset'";
     };
     extraConfig = {
       init.defaultBranch = lib.mkDefault "main";
