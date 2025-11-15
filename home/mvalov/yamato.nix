e6fe4ae714d79dff3786fc9cfa68729234d25bec
Miroslav Valov <91765698+mivalov@users.noreply.github.com>
2025-11-15 22:44:16 +0100

    Remove unnecessary code

--- home/mvalov/yamato.nix
+++ home/mvalov/yamato.nix
@@ -1,17 +1,8 @@
-{
-  config,
-  lib,
-  ...
-}:
+{ ... }:
 {
   imports = [
     ./global
     ./optional/bash.nix
     ./optional/git.nix
   ];
-  # when you want to override default bash feature configs
-  #config.features.bash = {
-  #  #ps1 = lib.concatStrings [];
-  #  #gitPrompt = {showDirtyState = true;};
-  #};
 }
