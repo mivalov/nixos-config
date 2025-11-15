c5aca4ec5593c9c47b34b50aa703922d7aa1f5ea
Miroslav Valov <91765698+mivalov@users.noreply.github.com>
2025-11-15 20:53:46 +0100

    Add shared module to inspect Home Manager options

--- hosts/common/global/home-manager.nix
+++ hosts/common/global/home-manager.nix
@@ -24,5 +24,42 @@
 
     backupFileExtension = "bak";
     extraSpecialArgs = { inherit inputs outputs; };
+
+    # https://www.reddit.com/r/NixOS/comments/1m0g1e9/get_options_for_specific_homemanager_module_user/
+    # Extra Home Manager modules that are applied to *every* HM user
+    sharedModules = [
+      # Home Manager module written as a function, takes module arguments (like options, lib, ...)
+      # and returns an attrset
+      (
+        { options, lib, ... }:
+        {
+          # define a new HM option `hmInternalOptions`,
+          # and lives under: config.home-manager.users.<user>.hmInternalOptions (inspect in a repl)
+          # This way one can inspect/debug custom feature definitions within the HM configs
+          # e.g. options.features.bash.gitPrompt
+          options.hmInternalOptions = lib.mkOption {
+            # attribute set of anything, flexible enough to hold the entire HM options tree
+            type = lib.types.attrsOf lib.types.anything;
+
+            # copies the full HM options tree for the user into `hmInternalOptions`
+            default = options;
+
+            # in docs, show/render literally `options` as Nix code
+            defaultText = lib.literalExpression "options";
+
+            description = "Internal: expose the full Home Manager options tree for REPL introspection.";
+
+            # marks this option as internal (for tools, etc)
+            internal = true;
+
+            # hide it from normal option listings/docs, keeps `home-manager options` output clean
+            visible = false;
+
+            # not meant to be overriden, only there for REPL introspection
+            readOnly = true;
+          };
+        }
+      )
+    ];
   };
 }
