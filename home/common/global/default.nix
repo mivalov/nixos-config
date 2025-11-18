d2557497598a815ad430238ce2cb8ce1b1995707
Miroslav Valov <91765698+mivalov@users.noreply.github.com>
2025-11-18 22:56:45 +0100

    Add global garbage collection config for home-manager
    This is necessary because nix-collect-garbage does not clean up
    home-manager generations (user profiles in XDG directories) when run as root.
    More info: https://github.com/NixOS/nix/issues/8508

--- home/common/global/default.nix
+++ home/common/global/default.nix
@@ -15,5 +15,14 @@
         "flakes"
       ];
     };
+    # Necessary due to https://github.com/NixOS/nix/issues/8508
+    gc = {
+      automatic = lib.mkDefault true;
+      frequency = lib.mkDefault "weekly";
+      # In HM release 25.11, frequency will be renamed to dates
+      # TODO: After upgrading to the new HM release, replace "frequency" with "dates"
+      # dates = lib.mkDefault "weekly";
+      options = lib.mkDefault "--delete-older-than 30d";
+    };
   };
 }
