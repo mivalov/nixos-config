f4fde8540938e5cefa6b8e42c20716ab07ce3a61
Miroslav Valov <91765698+mivalov@users.noreply.github.com>
2025-11-23 23:36:18 +0100

    Add system-level gnome configs

---
+++ hosts/common/optional/gnome.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  # https://wiki.nixos.org/wiki/GNOME
+  # Enable the GNOME Desktop Environment.
+  services.xserver = {
+    # GNOME Display Manager (GDM)
+    displayManager.gdm = {
+      enable = lib.mkDefault true;
+      autoSuspend = lib.mkDefault false;
+    };
+    # GNOME Desktop Manager
+    desktopManager.gnome = {
+      enable = lib.mkDefault true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    gnome-tweaks
+    dconf-editor
+  ];
+}
--- hosts/titania/default.nix
+++ hosts/titania/default.nix
@@ -7,6 +7,7 @@
   imports = [
     ./hardware-configuration.nix
     ../common/global
+    ../common/optional/gnome.nix
     ../common/users/mvalov
     ./configuration.nix
   ];
