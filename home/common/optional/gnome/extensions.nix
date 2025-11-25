d3c1fb1d155cbfb37802f934a1d0ad7b877296d9
Miroslav Valov <91765698+mivalov@users.noreply.github.com>
2025-11-25 02:53:26 +0100

    Add default configs for specific extensions

--- home/common/optional/gnome/extensions.nix
+++ home/common/optional/gnome/extensions.nix
@@ -5,7 +5,7 @@
   ...
 }:
 let
-  inherit (lib.hm.gvariant) mkTuple;
+  inherit (lib.hm.gvariant) mkInt32;
   inherit (lib) mkDefault;
 in
 {
@@ -27,6 +27,37 @@ in
           transparent-top-bar-adjustable-transparency.extensionUuid
           weather-oclock.extensionUuid
         ];
+      };
+
+      "com/ftpix/transparentbar" = {
+        transparency = mkDefault 25;
+      };
+
+      "org/gnome/shell/extensions/dash-to-dock" = {
+        apply-custom-theme = mkDefault false;
+        autohide-in-fullscreen = mkDefault false;
+        background-opacity = mkDefault 0.25;
+        click-action = mkDefault "minimize-or-previews";
+        custom-theme-shrink = mkDefault false;
+        dash-max-icon-size = mkDefault 48;
+        dock-fixed = mkDefault false;
+        dock-position = mkDefault "BOTTOM";
+        height-fraction = mkDefault 0.9;
+        icon-size-fixed = mkDefault false;
+        intellihide-mode = mkDefault "FOCUS_APPLICATION_WINDOWS";
+        middle-click-action = mkDefault "launch";
+        multi-monitor = mkDefault true;
+        preferred-monitor = mkDefault (-2); # active/focused monitor (dynamic)
+        #preferred-monitor-by-connector = mkDefault "eDP-1";
+        running-indicator-style = mkDefault "DASHES";
+        shift-click-action = mkDefault "minimize";
+        shift-middle-click-action = mkDefault "launch";
+        show-trash = mkDefault false;
+        transparency-mode = mkDefault "FIXED";
+      };
+
+      "org/gnome/shell/extensions/weather-oclock" = {
+        weather-after-clock = mkDefault false;
       };
     };
   };
