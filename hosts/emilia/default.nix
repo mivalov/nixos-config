a51cacb81057a789101a67f20e86125ebea9598e
Miroslav Valov <91765698+mivalov@users.noreply.github.com>
2026-03-31 21:53:21 +0200

    Enable auto-upgrade feature on emilia

--- hosts/emilia/default.nix
+++ hosts/emilia/default.nix
@@ -7,13 +7,21 @@
   imports = [
     ./hardware-configuration.nix
     ../common/global
-    ../common/optional/gnome.nix
     ../common/optional/amd_gpu.nix
+    ../common/optional/auto-upgrade.nix
     ../common/optional/gaming.nix
+    ../common/optional/gnome.nix
     ../common/users/pvalov
     ./configuration.nix
   ];
 
   # Use standard Bulgarian keyboard layout
   services.xserver.xkb.variant = ",,";
+
+  features.auto-upgrade = {
+    enable = true;
+    user = "pvalov";
+    flakePath = "/home/pvalov/workspace/git_projects/nixos-config";
+    dates = "daily";
+  };
 }
