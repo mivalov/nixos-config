5fe2697d0d61c84c241acfdfe62bd28026be0101
Miroslav Valov <91765698+mivalov@users.noreply.github.com>
2026-07-18 23:09:05 +0200

    Add more mangohud settings

--- home/common/optional/gaming/mangohud.nix
+++ home/common/optional/gaming/mangohud.nix
@@ -3,14 +3,27 @@
   ...
 }:
 {
-  # https://github.com/flightlessmango/MangoHud
   # FPS overlay
+  # https://github.com/flightlessmango/MangoHud
   programs.mangohud = {
     # Launch option: mangohud %command%
     enable = lib.mkDefault true;
     settings = {
       # https://raw.githubusercontent.com/flightlessmango/MangoHud/master/data/MangoHud.conf
+      # To disable parameters enabled by default, set them explicitly to 0 (e.g. fps=0)
+      cpu_stats = lib.mkDefault true;
+      cpu_temp = lib.mkDefault true;
+      fps = lib.mkDefault true;
+      frametime = lib.mkDefault true;
+      frame_timing = lib.mkDefault true;
       gpu_name = lib.mkDefault true;
+      gpu_stats = lib.mkDefault true;
+      gpu_temp = lib.mkDefault true;
+      position = lib.mkDefault "bottom-right";
+      ram = lib.mkDefault true;
+      text_outline = lib.mkDefault true;
+      vram = lib.mkDefault true;
+      wine = lib.mkDefault true;
     };
   };
 }
