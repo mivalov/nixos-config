469960a045a414d6af0ccb4a6ccf8f52c41e6011
Miroslav Valov <91765698+mivalov@users.noreply.github.com>
2026-09-25 12:20:33 +0200

    Add zed and dconf settings to titania

--- home/mvalov/titania.nix
+++ home/mvalov/titania.nix
@@ -10,10 +10,23 @@
     ../common/optional/gnome
     ../common/optional/fonts.nix
     ../common/optional/office.nix
+    ../common/optional/zed-editor.nix
   ];
 
   home.packages = with pkgs; [
     rustdesk-flutter
     vlc
   ];
+
+  dconf.settings = {
+    "org/gnome/shell" = {
+      favorite-apps = [
+        "firefox.desktop"
+        "org.gnome.Console.desktop"
+        "dev.zed.Zed.desktop"
+        "org.gnome.Nautilus.desktop"
+        "org.gnome.Settings.desktop"
+      ];
+    };
+  };
 }
